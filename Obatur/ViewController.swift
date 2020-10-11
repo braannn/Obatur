68de29cc74b556d936cf8ad500bf711d36e1a32e
braannn <65325830+braannn@users.noreply.github.com>
2020-10-11 13:24:54 +0700

    Initial commit

---
+++ Obatur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Obatur
+//
+//  Created by Muhammad Syabran on 09/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
