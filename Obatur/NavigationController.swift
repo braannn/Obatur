c6db5aa342c825603f94bad00cd5d4f51036d32e
braannn <65325830+braannn@users.noreply.github.com>
2020-10-13 07:25:29 +0700

    fix navigation bar for large title and search bar

--- Obatur/NavigationController.swift
+++ Obatur/NavigationController.swift
@@ -11,15 +11,10 @@ class NavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavBar()
+        
+        
     }
 
-    func setupNavBar() {
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        let searchController = UISearchController(searchResultsController: nil)
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
-    }
+    
 
 }
