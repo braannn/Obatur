df1ac18d6ddf215d338f34c5972023d2255178b6
Ricky Andi Kurniawan <rickyandi64@gmail.com>
2020-10-17 16:33:57 +0700

    Preseption screen

---
+++ Obatur/Prescription ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Prescription ViewController.swift
+//  Obatur
+//
+//  Created by Ricky Andi Kurniawan on 17/10/20.
+//
+
+import UIKit
+
+class Prescription_ViewController: UIViewController  {
+    @IBOutlet weak var patientusername: UILabel!
+    @IBOutlet weak var patientage: UILabel!
+    @IBOutlet weak var patientmedicine: UILabel!
+    @IBOutlet weak var aturanminum: UILabel!
+    @IBOutlet weak var berapakalisehari: UILabel!
+    @IBOutlet weak var catatan: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func button(_ sender: Any) {
+    }
+    
+}
