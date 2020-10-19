72f7499b643d82427685205e1449bf2392a40212
braannn <65325830+braannn@users.noreply.github.com>
2020-10-19 18:18:37 +0700

    1. fix contraint and removed navigation, karena tabrakan dgn popup 2. custom button appereance 3. add VC for popover obat detail screen

--- Obatur/PrescriptionScreen.swift
+++ Obatur/PrescriptionScreen.swift
@@ -11,6 +11,7 @@ class PrescriptionScreen: UIViewController {
     
     @IBOutlet weak var namaPasienTxt: UILabel!
     @IBOutlet weak var umurPasienTxt: UILabel!
+    @IBOutlet weak var btnKirimOutlet: UIButton!
     
     var namaPasien = ""
     var umurPasien = ""
@@ -21,5 +22,14 @@ class PrescriptionScreen: UIViewController {
         
         self.namaPasienTxt.text = namaPasien
         self.umurPasienTxt.text = umurPasien
+        
+        // custom button
+        // corner radius
+        btnKirimOutlet.layer.cornerRadius = 10
+        // shadow
+        btnKirimOutlet.layer.shadowColor = UIColor.black.cgColor
+        btnKirimOutlet.layer.shadowOffset = CGSize(width: 0, height: 0)
+        btnKirimOutlet.layer.shadowOpacity = 0.3
+        btnKirimOutlet.layer.shadowRadius = 4.0
     }
 }
